ac49bf7a4851fcd136c2071dc63d9d7eeb661c38
Holgenberg <alexeiholgate1@gmail.com>
2019-09-24 14:40:51 +0100

    Created Deliminator Setter

--- NU.StringCalculatorExercise/NU.StringCalculatorExercise/Program.cs
+++ NU.StringCalculatorExercise/NU.StringCalculatorExercise/Program.cs
@@ -6,20 +6,22 @@ namespace NU.StringCalculatorExercise
     {
         static void Main(string[] args)
         {
-            //var sum = Add("1\n2,3");
-            //Console.WriteLine(sum);
-            //Console.ReadKey();
+            var sum = Add("1, 2, 3");
+            Console.WriteLine(sum);
+            Console.ReadKey();
         }
 
         static int Add(string numbers)
         {
             var sum = 0;
 
-            var numbersStringArray = numbers.Split(',');
+            var deliminator = SetDeliminator(numbers);
+
+            var numbersStringArray = numbers.Split(deliminator);
 
             foreach (var numberString in numbersStringArray)
             {
-                if (numberString.Contains('\n'))
+                if (numberString.Contains("\n"))
                 {
                     sum += AddMultipleNumbersFromString(numberString);
                 }
@@ -33,11 +35,26 @@ namespace NU.StringCalculatorExercise
             return sum;
         }
 
+        static string SetDeliminator(string numbers)
+        {
+            var deliminator = ",";
+
+            if (numbers.StartsWith("//") && numbers.Contains("\n"))
+            {
+                var startIndexOfDelim = 2;
+                var endIndexOfDelim = numbers.IndexOf("\n");
+
+                deliminator = numbers.Substring(startIndexOfDelim, endIndexOfDelim - startIndexOfDelim);
+            }
+
+            return deliminator;
+        }
+
         static int AddMultipleNumbersFromString(string multipleNumberString)
         {
             var numberStringSumAdditive = 0;
 
-            var multipleNumberStringArray = multipleNumberString.Split('\n');
+            var multipleNumberStringArray = multipleNumberString.Split("\n");
 
             foreach (var numberString in multipleNumberStringArray)
             {
